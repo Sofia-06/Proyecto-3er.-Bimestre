c5c9de07678e416cd473c01fafac162eb7c59a3e
Sofia Abigail Sabán Cárcamo <201800090@colsanjose.edu.gt>
2020-07-15 10:10:05 -0600

    Terminado

---
+++ Proyecto_3er.Bimestre/Proyecto_3er.Bimestre/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Proyecto_3er.Bimestre
+{
+    class Program
+    {
+        static SerialPort Arduino = new SerialPort("COM27", 9600);
+        static void Main(string[] args)
+        {
+            int num;
+            Arduino.Parity = Parity.None;
+            Arduino.StopBits = StopBits.One;
+            Arduino.DataBits = 8;
+            Arduino.Handshake = Handshake.None;
+            Arduino.RtsEnable = true;
+
+            Arduino.Open();
+            // Grupo de Trabajo: Fatima Hernandez, Sofia Sabán, Keyli Solis
+            do
+            {
+                Console.WriteLine("A continuación escoja la acción que desea llevar a cabo ");
+                Console.WriteLine("1. Encender LED color rojo ");
+                Console.WriteLine("2. Apagar LED color rojo ");
+                Console.WriteLine("3. Encender LED color naranja ");
+                Console.WriteLine("4. Apagar LED color naranja: ");
+                Console.WriteLine("5. Salir");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+
+
+                if (num == 1)
+                {
+                    Console.WriteLine("Com abierto");
+                    byte[] data = Encoding.ASCII.GetBytes("z");
+                    Arduino.Write(data, 0, data.Length);
+                    Console.Clear();
+                }
+                else if (num == 2)
+                {
+                    Console.WriteLine("Com abierto");
+                    byte[] data = Encoding.ASCII.GetBytes("i");
+                    Arduino.Write(data, 0, data.Length);
+                    num = 1;
+                    Console.Clear();
+                }
+
+                if (num == 3)
+                {
+                    Console.WriteLine("Com abierto");
+                    byte[] data1 = Encoding.ASCII.GetBytes("p");
+                    Arduino.Write(data1, 0, data1.Length);
+                    num = 1;
+                    Console.Clear();
+                }
+                else if (num == 4)
+                {
+                    Console.WriteLine("Com abierto");
+                    byte[] data = Encoding.ASCII.GetBytes("l");
+                    Arduino.Write(data, 0, data.Length);
+                    num = 1;
+                    Console.Clear();
+                }
+
+                if (num == 5)
+                {
+                    Console.Clear();
+                    break;
+                }
+
+            } while (num == 1);
+            Console.ReadKey();
+            Arduino.Close();
+        }
+    }
+}
+
+//https://www.tinkercad.com/things/5FiJXwGohzy-epic-jofo/editel?sharecode=BSlwTkhea7stCNLDo_bg5kVWt5hoDlzWdWnsOcHeDfM
+        
